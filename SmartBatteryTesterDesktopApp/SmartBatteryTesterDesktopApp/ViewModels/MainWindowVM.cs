5f139a955bb093949e6bc8a48596e157b6e50de8
Siarhei Vasileuski <vasileuski.sergei@gmail.com>
2022-10-30 20:12:35 +0300

    feat: add commands

--- SmartBatteryTesterDesktopApp/SmartBatteryTesterDesktopApp/ViewModels/MainWindowVM.cs
+++ SmartBatteryTesterDesktopApp/SmartBatteryTesterDesktopApp/ViewModels/MainWindowVM.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using SmartBatteryTesterDesktopApp.Commands;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
@@ -8,14 +9,25 @@ namespace SmartBatteryTesterDesktopApp.ViewModels
     internal class MainWindowVM : INotifyPropertyChanged
     {
 
-        private List<string> _portNameListVM = new List<string>() { "COM 1", "COM 2", "COM 3", "COM 4", "COM 5"};
+        private List<string> _portNameListVM = new List<string>() { "COM 1", "COM 2", "COM 3", "COM 4", "COM 5"}; // TODO: temp
         public List<string> PortNameListVM
         {
             get => _portNameListVM;
 
         }
 
-        
+        private RelayCommand _connectToComPortCommand;
+        public RelayCommand ConnectToComPortCommand
+        {
+            get
+            {
+                return _connectToComPortCommand ??
+                    (_connectToComPortCommand = new RelayCommand(obj =>
+                    {
+                        MessageBox.Show("Command Test");    // TODO: temp
+                    }));
+            }
+        }
 
 
         public event PropertyChangedEventHandler PropertyChanged;
