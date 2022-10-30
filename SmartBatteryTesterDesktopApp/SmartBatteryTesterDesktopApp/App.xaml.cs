0829254d268f891bdb79aaf34e9cf45bb479e3ab
Siarhei Vasileuski <vasileuski.sergei@gmail.com>
2022-10-30 19:05:37 +0300

    feat: add main window markup

---
+++ SmartBatteryTesterDesktopApp/SmartBatteryTesterDesktopApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Ninject;
+using SmartBatteryTesterDesktopApp.ViewModels;
+using System.Windows;
+
+namespace SmartBatteryTesterDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static IKernel kernel;
+        public static IKernel Kernel => kernel;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            kernel = new StandardKernel();
+
+ 
+
+            var appVM = kernel.Get<MainWindowVM>();
+
+            MainWindow = new MainWindow();
+            MainWindow.DataContext = appVM;
+            MainWindow.Show();
+        }
+    }
+}
---
+++ SmartBatteryTesterDesktopApp/SmartBatteryTesterDesktopApp/ViewModels/MainWindowVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SmartBatteryTesterDesktopApp.ViewModels
+{
+    internal class MainWindowVM
+    {
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string prop = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
