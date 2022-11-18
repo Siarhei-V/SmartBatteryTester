d1badc55e638845e8cbc3fedf5d6d265479dbb93
Siarhei Vasileuski <vasileuski.sergei@gmail.com>
2022-11-18 20:57:15 +0300

    feat: add continuous measurement chart

--- SmartBatteryTesterWebApp/SmartBatteryTesterWebApp.UI/Controllers/MeasurementController.cs
+++ SmartBatteryTesterWebApp/SmartBatteryTesterWebApp.UI/Controllers/MeasurementController.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SmartBatteryTesterWebApp.BLL.DTO;
+using SmartBatteryTesterWebApp.BLL.Interfaces;
+using SmartBatteryTesterWebApp.UI.Infrastructure;
+using SmartBatteryTesterWebApp.UI.Models;
 
 namespace SmartBatteryTesterWebApp.UI.Controllers
 {
     public class MeasurementController : Controller
     {
-        public IActionResult Index() => View();
+        IMeasurementOutputService _measurementService;
+        IMeasurementChartDataCreator _chartCreator;
+
+        public MeasurementController(IMeasurementOutputService measurementService, IMeasurementChartDataCreator chartCreator)
+        {
+            _measurementService = measurementService;
+            _chartCreator = chartCreator;
+        }
+
+        public IActionResult Index()
+        {
+            IMapper mapper;
+
+            List<MeasurementDTO> measurementDTOs = _measurementService.GetMeasurement(3);
+            mapper = new MapperConfiguration(m => m.CreateMap<MeasurementDTO, MeasurementViewModel>()).CreateMapper();
+            var measurements = mapper.Map<List<MeasurementDTO>, List<MeasurementViewModel>>(measurementDTOs);
+
+            var chartData = _chartCreator.GetLineChartData(measurements);
+
+            return View(chartData);
+        }
     }
 }
--- SmartBatteryTesterWebApp/SmartBatteryTesterWebApp.UI/Infrastructure/MeasurementsHub.cs
+++ SmartBatteryTesterWebApp/SmartBatteryTesterWebApp.UI/Infrastructure/MeasurementsHub.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using SmartBatteryTesterWebApp.UI.Models;
-using System.Text.Json;
 
 namespace SmartBatteryTesterWebApp.UI.Infrastructure
 {
     public class MeasurementsHub : Hub
     {
+        IMeasurementChartDataCreator _measurementChartDataCreator;
+
+        public MeasurementsHub(IMeasurementChartDataCreator measurementChartDataCreator)
+        {
+            _measurementChartDataCreator = measurementChartDataCreator;
+        }
+
         public async Task Send(string message, MeasurementViewModel measurement)
         {
-            await Clients.All.SendAsync("Receive", message, measurement);
+            var chartData = _measurementChartDataCreator.GetLineChartData(new List<MeasurementViewModel> () { measurement});
+            await Clients.All.SendAsync("Receive", message, chartData);
         }
     }
 }
