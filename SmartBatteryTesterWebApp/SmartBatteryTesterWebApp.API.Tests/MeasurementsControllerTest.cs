0da23e9879c799be6587d906bf6247f2e2c26b6b
Siarhei Vasileuski <vasileuski.sergei@gmail.com>
2022-11-19 17:53:43 +0300

    feat: add test project for api

---
+++ SmartBatteryTesterWebApp/SmartBatteryTesterWebApp.API.Tests/MeasurementsControllerTest.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace SmartBatteryTesterWebApp.API.Tests
+{
+    public class MeasurementsControllerTest
+    {
+
+
+        [Fact]
+        public void AddMeasurementSet_Check()
+        {
+
+        }
+    }
+}
