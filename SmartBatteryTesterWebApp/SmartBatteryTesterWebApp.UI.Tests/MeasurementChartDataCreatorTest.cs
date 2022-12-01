ad91634d45a665f2687b03cff4dc57792a19bccb
Siarhei Vasileuski <vasileuski.sergei@gmail.com>
2022-12-01 11:53:35 +0300

    ref: edit MeasurementChartDataCreatorTest

--- SmartBatteryTesterWebApp/SmartBatteryTesterWebApp.UI.Tests/MeasurementChartDataCreatorTest.cs
+++ SmartBatteryTesterWebApp/SmartBatteryTesterWebApp.UI.Tests/MeasurementChartDataCreatorTest.cs
@@ -20,9 +20,18 @@ namespace SmartBatteryTesterWebApp.UI.Tests
         public void GetLineChartData_CheckReturnedvalues()
         {
             // Arrange
+            var measurementViewModelList = new List<MeasurementViewModel>()
+            {
+                new MeasurementViewModel() { Voltage = 10, Current = 5, 
+                    MeasurementDateTime = new DateTime(1, 1, 1, 2, 2, 2).ToString()},
+                new MeasurementViewModel() { Voltage = 20, Current = 15, 
+                    MeasurementDateTime = new DateTime(3, 3, 3, 4, 4, 4).ToString()},
+                new MeasurementViewModel() { Voltage = 30, Current = 25, 
+                    MeasurementDateTime = new DateTime(5, 5, 5, 6, 6, 6).ToString()},
+            };
 
             // Act
-            var methodResult = _dataCreator.GetLineChartData(CreateMeasurementViewModelList());
+            var methodResult = _dataCreator.GetLineChartData(measurementViewModelList);
 
             // Assert
             Assert.NotNull(methodResult);
@@ -33,17 +42,5 @@ namespace SmartBatteryTesterWebApp.UI.Tests
             Assert.Equal(30, result.Datasets[0].Data[2]);
             Assert.Equal(new DateTime(5, 5, 5, 6, 6, 6).ToString(), result.Labels[2].ToString());
         }
-
-        #region Private Methods
-        private List<MeasurementViewModel> CreateMeasurementViewModelList()
-        {
-            return new List<MeasurementViewModel>()
-            {
-                new MeasurementViewModel() { Voltage = 10, Current = 5, MeasurementDateTime = new DateTime(1, 1, 1, 2, 2, 2).ToString()},
-                new MeasurementViewModel() { Voltage = 20, Current = 15, MeasurementDateTime = new DateTime(3, 3, 3, 4, 4, 4).ToString()},
-                new MeasurementViewModel() { Voltage = 30, Current = 25, MeasurementDateTime = new DateTime(5, 5, 5, 6, 6, 6).ToString()},
-            };
-        }
-        #endregion
     }
 }
